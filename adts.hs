c97d6172ca6481b74226eb09b5559b90952b090d
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-12-23 23:30:14 -0200

    trees

--- adts.hs
+++ adts.hs
@@ -100,3 +100,68 @@ data Programmer = Programmer {
 
 allProgrammers :: [Programmer]
 allProgrammers = [Programmer s l | s <- allOperatingSystems, l <- allLanguages]
+
+
+
+data Tree a = Leaf | Node (Tree a) a (Tree a)
+      deriving (Eq, Ord, Show)
+
+
+insert :: (Ord a) =>  a -> Tree a -> Tree a
+insert a Leaf = Node Leaf a Leaf
+insert a (Node left c right  )
+  | a == c = Node left a right
+  | a < c =  Node ( insert a left) c right
+  | a > c =  Node left c (insert a right)
+
+
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) = Node (mapTree f left) (f a) (mapTree f right)
+
+
+testTree' :: Tree Integer
+testTree' = Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+mapExpected = Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+mapOkay = if mapTree (+1) testTree' == mapExpected
+          then print "yup okay!"
+          else error "test failed!"
+
+
+testTreeList = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+preorder :: Tree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++  (preorder left) ++ (preorder right)
+
+inorder :: Tree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = (preorder left) ++ [a] ++ (preorder right)
+
+postorder :: Tree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = (preorder left) ++ (preorder right) ++ [a]
+
+
+testPreorder :: IO ()
+testPreorder = if preorder testTreeList == [2, 1, 3]
+               then putStrLn "Preorder fine!"
+               else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder = if inorder testTreeList == [1, 2, 3]
+              then putStrLn "Inorder fine!"
+              else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder = if postorder testTreeList == [1, 3, 2]
+                then putStrLn "Postorder fine!"
+                else putStrLn "postorder failed check"
+
+testTreeMain :: IO ()
+testTreeMain = do
+  testPreorder
+  testInorder
+  testPostorder
