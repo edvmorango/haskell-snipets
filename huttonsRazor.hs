3bf654925d1bfa56612e1737825772c3b7d7c72f
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-12-26 23:59:01 -0200

    Hutton's Razor

--- huttonsRazor.hs
+++ huttonsRazor.hs
@@ -5,8 +5,10 @@ data Expr =
    Lit Integer
  | Add Expr Expr
 
-
-
 eval :: Expr -> Integer
 eval (Lit a) = a
 eval (Add x y) = eval x + eval y
+
+printExpr :: Expr -> String
+printExpr (Lit a) = show a
+printExpr (Add x y) = printExpr x ++ " + " ++ printExpr y
