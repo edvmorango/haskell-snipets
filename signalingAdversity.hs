16d5cbe559862e95ae225bbae193192256acd8ed
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-12-27 23:32:53 -0200

    self recursive lambda :)

--- signalingAdversity.hs
+++ signalingAdversity.hs
@@ -8,11 +8,6 @@ notThe :: String -> Maybe String
 notThe "the" = Nothing
 notThe a = Just a
 
--- split :: (Eq a) => a -> [a] -> [a]
--- split _ [] = []
--- split del lis =  takeWhile (/=del) lis : split del next
-  -- where next = (drop 1 $ dropWhile(/=del) lis)
-
 split :: Char -> String -> [String]
 split _ [] = []
 split del s = takeWhile (/=del) s : split del next
@@ -37,3 +32,26 @@ isVowel = ( (flip elem "aeiou") .  toLower)
 
 countVowels :: String -> Integer
 countVowels = foldr (\a acc ->  if isVowel a then acc + 1 else acc) 0
+
+newtype Word' = Word' String deriving (Eq, Show)
+
+mkWord :: String ->  Maybe Word'
+mkWord s = if a > b
+         then Nothing
+         else Just (Word' s)
+  where (a,b) = foldr (\a (v,c) -> if (isVowel a) then (v+1,c) else (v, c+1)) (0,0) s
+
+data Nat = Zero | Succ Nat deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger (Zero) = 0
+natToInteger (Succ a) = 1 + natToInteger a
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat a
+  | a < 0 = Nothing
+  | otherwise = Just $ parse a
+              where parse 0 = Zero
+                    parse p = Succ $ parse (p-1)
+
+--
