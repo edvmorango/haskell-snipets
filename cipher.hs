2a492cf3f1f56885cff6ec16e71bdc1504f77b6e
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-12-30 23:10:58 -0200

    improvement cipher

--- cipher.hs
+++ cipher.hs
@@ -9,3 +9,21 @@ encode c a = map (\x ->  chr $ (ord x) + c ) a
 
 decode :: Shift -> String -> String
 decode c a = map (\x ->  chr $ (ord x) - c ) a
+
+
+dynamicEncode :: IO ()
+dynamicEncode = do
+  putStr "Put the Shift size: "
+  size <- getLine
+  putStr "Put a word to encode: "
+  word <- getLine
+  putStrLn $ encode (read size :: Int) word
+
+
+dynamicDecode :: IO ()
+dynamicDecode = do
+  putStr "Put the Shift size: "
+  size <- getLine
+  putStr "Put a word to decode: "
+  word <- getLine
+  putStrLn $ decode (read size :: Int) word
