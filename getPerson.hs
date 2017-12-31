b8ff3c5c658f16eaa7a5a81715e301cc5637f84f
J. Eduardo V. Morango <edvmorango@gmail.com>
2017-12-31 00:00:48 -0200

    person

---
+++ getPerson.hs
@@ -0,0 +1,31 @@
+module GetPerson where
+
+type Name = String
+type Age = Integer
+
+data Person = Person Name Age deriving Show
+
+data PersonInvalid =
+    NameEmpty
+  | AgeTooLow
+  | PersonInvalidUnknown String deriving (Eq, Show)
+
+
+mkPerson :: Name -> Age -> Either PersonInvalid Person
+mkPerson name age
+  | name /= "" && age > 0 = Right $ Person name age
+  | name == "" = Left NameEmpty
+  | not (age > 0) = Left AgeTooLow
+  | otherwise =
+    Left $ PersonInvalidUnknown $ "Name was: " ++ show name ++ " Age was: " ++ show age
+
+
+gimmePerson :: IO ()
+gimmePerson = do
+  putStr "Name: "
+  name <- getLine
+  putStr "Age: "
+  age <- getLine
+  case (mkPerson name (read age :: Integer)) of
+    Left e -> putStrLn $ "Error: " ++ show e
+    Right p -> putStrLn $ "Success: " ++ (show p)
