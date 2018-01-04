602289a82e433ceba13ba037d25488e893f9a6eb
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-04 02:12:31 -0200

    need fixes

--- tests-project/test/ExercisesTests.hs
+++ tests-project/test/ExercisesTests.hs
@@ -5,10 +5,28 @@ import Test.QuickCheck
 import Exercises
 
 
+genFractional :: (Arbitrary a , Fractional a) => Gen a -- Must extends Arbitrary when wants force a Instance
+genFractional = do
+  a <- arbitrary
+  return a
+
+genOrd :: (Arbitrary a, Ord a) =>  Gen a
+genOrd = do
+  a <- arbitrary
+  return a
+
+genListOfOrd :: (Ord a) => Gen a -> Gen [a]
+genListOfOrd a = listOf a
 
 
 exercisesTests :: IO ()
 exercisesTests = hspec $ do
   describe "half" $ do
     it "half N = N / 2" $ do
-      forAll arbitrarySizedFractional (\x ->  half x == (x / 2) )
+      forAll (genFractional :: Gen Double ) (\x ->  half x == (x / 2) )
+  describe "halfIdentity" $ do
+    it "halfIdentity N = N " $ do
+      forAll (genFractional :: Gen Double ) (\x -> halfIdentity x ==  x )
+  describe "listOrdered" $ do
+    it "listOrdered == True when ordered" $ do
+      forAll (genListOfOrd (genOrd :: Gen Int) ) (\x -> listOrdered x  == True)
--- tests-project/test/tests.hs
+++ tests-project/test/tests.hs
@@ -1,6 +1,4 @@
 module Main where
-
-
 import RecursionTests
 import ExercisesTests
 
