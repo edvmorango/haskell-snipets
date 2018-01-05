e577dc8d16995ff35293d54656469e21ac443922
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-04 22:23:29 -0200

    first phase

--- tests-project/test/ExercisesTests.hs
+++ tests-project/test/ExercisesTests.hs
@@ -3,7 +3,7 @@ module ExercisesTests (exercisesTests) where
 import Test.Hspec
 import Test.QuickCheck
 import Exercises
-
+import Data.List
 
 genFractional :: (Arbitrary a , Fractional a) => Gen a -- Must extends Arbitrary when wants force a Instance
 genFractional = do
@@ -15,8 +15,16 @@ genOrd = do
   a <- arbitrary
   return a
 
-genListOfOrd :: (Ord a) => Gen a -> Gen [a]
-genListOfOrd a = listOf a
+genSortedList :: (Ord a) => Gen a -> Gen [a]
+genSortedList a = do
+   l <- listOf a
+   return $ sort l
+
+genNumber :: (Arbitrary a, Eq a, Num a) => Gen a
+genNumber = do
+  a <- arbitrary
+  return a
+
 
 
 exercisesTests :: IO ()
@@ -29,4 +37,16 @@ exercisesTests = hspec $ do
       forAll (genFractional :: Gen Double ) (\x -> halfIdentity x ==  x )
   describe "listOrdered" $ do
     it "listOrdered == True when ordered" $ do
-      forAll (genListOfOrd (genOrd :: Gen Int) ) (\x -> listOrdered x  == True)
+      forAll (genSortedList (genOrd :: Gen Int)) (\x -> listOrdered x)
+  describe "plusAssociative" $ do
+    it "plusAssociative a b c = true" $ do
+      forAll (genNumber :: Gen Int) (\x y z -> plusAssociative x y z)
+  describe "plusCommutative" $ do
+    it "plusCommutative a b c = true" $ do
+      forAll (genNumber :: Gen Int) (\x y -> plusCommutative x y)
+  describe "productAssociative" $ do
+    it "productAssociative a b c = true" $ do
+      forAll (genNumber :: Gen Int) (\x y z -> productAssociative x y z)
+  describe "productCommutative" $ do
+    it "productCommutative a b c = true" $ do
+      forAll (genNumber :: Gen Int) (\x y -> productCommutative x y)
