4b37ff1aa126caa1402888abd9bef56d51e5f938
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-06 21:45:52 -0200

    finished tests

--- ciphers/test/Spec.hs
+++ ciphers/test/Spec.hs
@@ -16,15 +16,27 @@ genChar = elements $ concat [['a'..'z'],['A'..'Z'],[' ']]
 genWord :: Gen String
 genWord = listOf genChar
 
+genShift :: Gen Int
+genShift =  (genT :: Gen Int) `suchThat` (>0)
+
+genCaesarTest :: Gen (String, Int)
+genCaesarTest = do
+  w <- genWord
+  s <- genShift
+  return (w,s)
+
+
+caesarTest :: (String, Int) -> Bool
+caesarTest (w, s) = w == decoded
+  where encoded = C.encode s w
+        decoded = C.decode s encoded
+
+
 main :: IO ()
 main = hspec $ do
   describe "Caesar cipher" $ do
-    it "should encode valid string" $ do
-      True
-    it "should decode valid string" $ do
-      True
+    it "should encode and decode valid string" $ do
+      forAll (genCaesarTest) caesarTest
   describe "Vinegere Cipher" $ do
     it "should encode valid string" $ do
-      True
-    it "shoud decode valid string" $ do
-      True
+      V.encode (V.makeShift "ALLY" ) "MEET AT DAWN" `shouldBe` "MPPR AE OYWY"
