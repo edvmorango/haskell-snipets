adcf54f8c7b6f5aca1c989f3d542a4d32fd1743a
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-06 21:06:16 -0200

    finshed tests, can be improved if I use my customs generators, atleast they was useful for learning...

--- hangman/test/tests.hs
+++ hangman/test/tests.hs
@@ -3,6 +3,7 @@ module MainTest where
 import Hangman
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 
 main :: IO ()
 main = tests
@@ -17,11 +18,11 @@ genNaturalUntil :: Int -> Gen Int
 genNaturalUntil 0 = return 0
 genNaturalUntil a = elements [0..(a - 1)]
 
-genValidCharacters :: Gen Char
-genValidCharacters = elements ['a'..'z']
+genValidCharacter :: Gen Char
+genValidCharacter = elements ['a'..'z']
 
 genWord :: Gen String
-genWord = listOf (genValidCharacters) `suchThat` (\x -> let s = (length x) in  s >= minWordLength && s <= maxWordLength)
+genWord = listOf (genValidCharacter) `suchThat` (\x -> let s = (length x) in  s >= minWordLength && s <= maxWordLength)
 
 defaultHits :: String -> [Maybe Char]
 defaultHits word = map (\_ -> Nothing) word
@@ -34,6 +35,13 @@ genPuzzleChar = do
       char = word !! index in
     return  ( puzzle, char )
 
+genPuzzleWrongChar :: Gen (Puzzle, Char)
+genPuzzleWrongChar = do
+  word <- genWord  `suchThat` (/= "")
+  char <- genValidCharacter `suchThat` (not . flip elem word)
+  let puzzle = Puzzle word (defaultHits word) []
+    in return  (puzzle, char )
+
 
 fillCharacters :: String -> Puzzle -> Puzzle
 fillCharacters h p = foldr (\c w -> fillInCharacter w c) p h
@@ -43,28 +51,43 @@ genRightPuzzle :: Gen Puzzle
 genRightPuzzle = do
   word <- genWord  `suchThat` (/= "")
   index <-  genNaturalUntil (length word)
-  wrong <-   (listOf ((genValidCharacters) `suchThat` (\x -> not (elem x word))))  `suchThat` (\x -> length x < length word)
+  wrong <-   (listOf ((genValidCharacter) `suchThat` (\x -> not (elem x word))))  `suchThat` (\x -> length x < length word)
   return $ fillCharacters word (Puzzle word (map (Just) word) wrong)
 
 genWrongPuzzle :: Gen Puzzle
 genWrongPuzzle = do
   word <- genWord  `suchThat` (/= "")
   index <- genNaturalUntil (length word)
-  hits <- listOf (genValidCharacters `suchThat` (\x -> elem x word)) `suchThat` (\x -> length x < length word)
-  wrong <- (listOf ((genValidCharacters) `suchThat` (\x -> not (elem x word))))  `suchThat` (\x -> length x == length word)
+  hits <- listOf (genValidCharacter `suchThat` (\x -> elem x word)) `suchThat` (\x -> length x < length word)
+  wrong <- (listOf ((genValidCharacter) `suchThat` (\x -> not (elem x word))))  `suchThat` (\x -> length x == length word)
   return $ fillCharacters hits (Puzzle word (defaultHits word) wrong)
 
-
+genEmptyPuzzle :: Puzzle -> Puzzle
+genEmptyPuzzle (Puzzle w _ _) = Puzzle w (defaultHits w) []
 
 isFilled :: (Puzzle, Char) -> Bool
 isFilled (p,c) = result newPuzzle
   where newPuzzle = fillInCharacter p c
         result (Puzzle _ hits _)  = elem (Just c) hits
 
+handleRightGuess :: (Puzzle, Char) -> Property
+handleRightGuess (p,c) = monadicIO $ do
+  a <- run (handleGuess p c)
+  assert(response a)
+    where response (Puzzle _ hits _) = elem (Just c) hits
+
+handleWrongGuess :: (Puzzle, Char) -> Property
+handleWrongGuess (p,c) = monadicIO $ do
+  a <- run (handleGuess p c)
+  assert(response a)
+  where response (Puzzle _ hits guess) =  not (elem (Just c) hits)  && (elem c guess)
+
 tests :: IO ()
 tests =  hspec $ do
   describe "hangman" $ do
     it "should fill character if exists" $ do
       forAll genPuzzleChar isFilled
-    it "should handle the guess " $ do
-      True
+    it "should handle right guess " $ do
+      forAll genPuzzleChar handleRightGuess
+    it "should handle wrong guess " $ do
+      forAll genPuzzleWrongChar handleWrongGuess
