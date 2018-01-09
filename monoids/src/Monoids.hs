d3f7b94da09805174db7cac8205f48da901cb7b0
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-08 22:25:34 -0200

    I think that exercise is pointless...

--- monoids/src/Monoids.hs
+++ monoids/src/Monoids.hs
@@ -31,3 +31,24 @@ instance Monoid a => Monoid (Option a) where
   mappend (Algo a) (Algo b) = Algo $ a <> b
 
   mempty = Nada
+
+
+---------------
+
+type Verb = String
+type Adjective = String
+type Adverb = String
+type Noun = String
+type Exclamation = String
+
+
+madlibbin' :: Exclamation
+           -> Adverb
+           -> Noun
+           -> Adjective
+           -> String
+madlibbin' e adv noun adj =
+  mconcat [ e, "! he said " ,
+            adv, "as he jumped in to his car ",
+            noun, " and drove off with his " ,
+            adj, " wife."]
