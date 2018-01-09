885a4d25782a0820f12c084f060fc4615088ee74
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-08 22:41:27 -0200

    begin of tests

--- monoids/test/Spec.hs
+++ monoids/test/Spec.hs
@@ -1,2 +1,34 @@
+module Spec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Data.Monoid
+
+
+genThreple :: (Arbitrary a, Monoid a) => Gen (a,a,a)
+genThreple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return $ (a,b,c)
+
+
+genMonoid :: (Arbitrary a, Monoid a) => Gen a
+genMonoid = do
+  a <- arbitrary
+  return a
+
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "monoids" $ do
+    it "associativity " $ do
+      forAll (genThreple ::  Gen (Sum Int, Sum Int, Sum Int))
+        (\(a,b,c)  ->  (a <> b) <> c == a <> (b <> c))
+    it "associativity unitary" $ do
+     ((Sum 1 <> Sum 2) <> Sum 3) `shouldBe` (Sum 1 <> (Sum 2 <> Sum 3))
+    it "right identity" $ do
+      forAll (genMonoid :: Gen (Product Int)) (\a -> a <> mempty == a )
+    it "left identity" $ do
+      forAll (genMonoid :: Gen (Product Int)) (\a -> mempty <> a == a )
