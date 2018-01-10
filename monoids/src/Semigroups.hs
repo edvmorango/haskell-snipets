a4e6f59079984e7dddceb23b9c2cc43723b69215
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-01-09 22:07:52 -0200

    a miracle just happenede

--- monoids/src/Semigroups.hs
+++ monoids/src/Semigroups.hs
@@ -19,24 +19,41 @@ instance Semigroup a => Semigroup (Identity a) where
 instance Eq a => Eq (Identity a) where
   (Identity a) == (Identity a') = a == a'
 
+instance (Show a) => Show (Identity a) where
+  show (Identity a) = "Identity " ++ (show a)
+
+
+identity :: Eq a => Identity a -> Identity a -> Bool
+identity (Identity al) (Identity al') = al == al'
+
 
 semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
 semigroupAssoc a b c =  ((a <> b) <>c) == (a <> (b <> c))
 
-identity :: (Eq a) => Identity a -> Identity a -> Bool
-identity (Identity a ) (Identity a') =  a == a'
-
-
-
 type TrivAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+type IdentityType a =  Identity a -> Identity a -> Bool
+
+
+genT :: (Arbitrary a) => Gen a
+genT = do
+  a <- arbitrary
+  return a
 
 instance Arbitrary Trivial where
   arbitrary = return Trivial
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = do
+    b <- arbitrary
+    return $ Identity b
+
+
 
 tests :: IO ()
 tests = hspec $ do
    describe "Semigroup" $ do
     it "associativity" $ do
       quickCheck (semigroupAssoc :: TrivAssoc)
-    -- it "identity" $ do
-     -- forAll (genT :: Gen (Identity a, Identity a)) (\(Identity a, Identity b) -> a == b)
+    it "identity" $ do
+      quickCheck (identity :: (IdentityType Int))
