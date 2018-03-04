f9a416d83634be4912cf972d56aa508432a9bc87
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-03-04 15:13:17 -0300

    finished functors

--- functors/src/Functors.hs
+++ functors/src/Functors.hs
@@ -281,7 +281,55 @@ instance Functor (EvilGoateeConst x) where
   fmap f (GoatyConst a) = GoatyConst (f a)
 
 
-    
+
+
+data LiftItOut f a = LiftItOut (f a)
+
+instance (Functor fu) => Functor (LiftItOut fu) where 
+  fmap f (LiftItOut a) = LiftItOut (fmap f a)
+
+data Parappa f g a = DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa a b) = DaWrappa (fmap f a) (fmap f b)
+
+data IgnoreOne f g a b =  IgnoringSomething (f a) (g b)
+
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap f (IgnoringSomething a b) = IgnoringSomething (a) (fmap f b)
+
+
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious o a t) = Notorious (o) (a) (fmap f t)
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+instance Functor (List) where
+  fmap _ Nil = Nil
+  fmap f (Cons c (a)) = Cons (f c) (fmap f a)  
+
+
+
+data GoatLord a =
+    NoGoat 
+  | OneGoat a 
+  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor (GoatLord) where
+  fmap f (NoGoat) = NoGoat
+  fmap f (OneGoat a) = OneGoat (f a)
+  fmap f (MoreGoats a b c) = MoreGoats (fmap f a) (fmap f b) (fmap f c) 
+
+
+data TalkToMe a = Halt | Print String a | Read (String -> a)
+
+instance Functor (TalkToMe) where
+  fmap _ Halt = Halt
+  fmap f (Print s a) = Print s (f a) 
+  fmap f (Read l) = Read (fmap f l)
+
 
 
 getInt :: IO Int
