b88b4496632aace7e658f4a758ce6a768d66d374
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-03-07 00:49:33 -0300

    applicative  composition (must be refactored with functor Fun, instead of excessive use of pures

--- applicatives/src/Applicatives.hs
+++ applicatives/src/Applicatives.hs
@@ -18,17 +18,24 @@ genList a = do
   return as
 
 
+type FCompose = [Int] -> Fun Int Int -> Fun Int Int -> Bool
+
+funCompose :: (Eq (f c), Functor f) => f a -> Fun a b -> Fun b c -> Bool
+funCompose fu (Fun _ f)  (Fun _ g) = (fmap (g . f) fu) == ( (fmap g . fmap f) fu) 
+
+type ACompose = Maybe Int ->  (Fun  Int Int) -> Maybe (Fun Int Int) -> Bool
+
+apCompose :: (Eq (f c), Applicative f) => f a ->  (Fun a b ) -> (Fun b c) -> Bool
+apCompose v  (Fun _ f)  (Fun _ g) = ((pure (.) <*> (pure g) <*> (pure f) <*> v )) == ((pure g) <*> ( (pure f) <*> v))
+
 tests :: IO()
 tests = hspec $ do
   describe "Functors" $ do
-    it "id, fmapId and pureId are the same" $ do 
+    it "(Identity) -  id, fmapId and pureId are the same" $ do 
       forAll  (genList (genT :: Gen [Int])) 
         (\a -> let sid = id a  
                    fid = id <$> a
                    pid = (pure id) <*> a 
                in sid == fid && fid == pid )
-  
-    -- it "fmap id" $ do
-      -- forAll (genList (genT :: Gen [Int])) (\a ->  (id <$> a) == a )
-    
-  --  quickCheck (functorIdentity :: FunctorIdentityType Int )
+    -- it "(Composition)" $ do
+      -- forAll (genList ::   )
