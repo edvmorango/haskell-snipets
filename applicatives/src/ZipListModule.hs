da0acdd71b043ce4d79b0bf66da31812db780a8a
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-03-08 23:55:19 -0300

    properties

--- applicatives/src/ZipListModule.hs
+++ applicatives/src/ZipListModule.hs
@@ -17,6 +17,38 @@ instance (Show a, Monoid a) => Monoid (ZipList a) where
 -- instance Arbitrary a => Arbitrary (Sum a) where
 --   arbitrary = Sum <$> arbitrary
 
-
 instance Eq a => EqProp (ZipList a) where
   (=-=) = eq
+
+
+
+data List a = Nil | Cons a (List a) deriving (Eq, Show)
+
+take' :: Int -> List a -> List a
+take' _ (Cons a Nil) = Cons a Nil
+take' 1 (Cons a _) = Cons a Nil
+take' n (Cons a (next)) = Cons a (take' (n-1) next)
+
+
+instance Monoid (List a) where
+  mempty = Nil
+  mappend Nil a = a 
+  mappend (Cons a next) rl = Cons a (mappend next rl)
+  
+  
+instance Functor (List) where
+  fmap f (Nil)  = Nil 
+  fmap f (Cons a (next) ) =  Cons (f a ) (fmap f next) 
+
+instance Applicative (List) where
+  pure a = Cons a Nil
+  (<*>) Nil _ = Nil
+  (<*>) _ Nil = Nil
+  (<*>) (Cons f n) l  = (fmap f l) <> (n <*> l) 
+       
+
+-- tests :: IO()
+-- tests = hspec $ do
+--   describe "ZipList tests" $ do
+--     it "take " $ do
+--        True ==  True
