41be4bc57aeaa44050a9a2b891fdbb1f9f444e08
J. Eduardo V. Morango <edvmorango@gmail.com>
2018-03-14 22:55:00 -0300

    pair applicative/checker

--- applicatives/src/Exercises.hs
+++ applicatives/src/Exercises.hs
@@ -1,7 +1,9 @@
 module Exercises where
   
-
-
+import Test.Hspec  
+import Test.QuickCheck 
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes  
 
 lPure :: a -> [a]
 lPure = pure
@@ -28,4 +30,36 @@ arrowPure :: b -> (a -> b)
 arrowPure = pure
 
 arrowAp :: (a -> ( b -> c)) -> (a -> b) -> (a -> c)
-arrowAp = (<*>) 
+arrowAp = (<*>)
+
+data Pair a = Pair a a deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair a a') = Pair (f a) (f a') 
+  
+instance Applicative Pair where
+  pure a = Pair a a
+  (<*>) (Pair f f') (Pair a a') =  Pair (f a) (f' a')
+  
+  
+genPair :: ( Arbitrary a) => Gen (Pair a) 
+genPair = do
+  a' <- arbitrary
+  a <- arbitrary
+  return (Pair a a')
+    
+instance (Arbitrary a) => Arbitrary (Pair a) where
+  arbitrary = genPair
+
+-- Checkers
+
+
+instance (Eq a) => EqProp (Pair a) 
+  where (=-=) = eq  
+
+tests :: IO()
+tests = hspec $ do
+  describe "Laws" $ do
+    it "Pair applicative" $ do
+      quickBatch (applicative (Pair ("C1", "C2", "C3") ("C4", "C5", "C6") :: Pair (String, String, String))   )
+        
